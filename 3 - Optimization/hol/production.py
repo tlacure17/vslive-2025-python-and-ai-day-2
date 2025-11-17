b5d1117e2257b3fe0455ddd4586b68e0761b4e8b
Tyler Lacure <lacure.tyler@gmail.com>
2025-11-17 14:28:59 -0500

    optimization

--- 3 - Optimization/hol/production.py
+++
@@ -1,2 +0,0 @@
-# 3 - Optimization - production
-
---
+++ 3 - Optimization/hol/production/production.py
@@ -0,0 +1,23 @@
+import scipy.optimize
+
+# Objective Function: 50x_1 + 80x_2     Cost to Run
+# Constraint 1: 5x_1 + 2x_2 <= 20       Have 20 Units of Labor
+# Constraint 2: -10x_1 + -12x_2 <= -90  Need 90 Units of Output
+
+# result = scipy.optimize.linprog(
+#     [50, 80],  # Cost function: 50x_1 + 80x_2
+#     A_ub=[[5, 2], [-10, -12]],  # Coefficients for inequalities
+#     b_ub=[20, -90],  # Constraints for inequalities: 20 and -90
+# )
+
+result = scipy.optimize.linprog(
+    [75, 80],  # Cost function: 50x_1 + 80x_2
+    A_ub=[[5, 2], [-10, -12]],  # Coefficients for inequalities
+    b_ub=[20, -90],  # Constraints for inequalities: 10 and -90
+)
+
+if result.success:
+    print(f"X1: {round(result.x[0], 2)} hours")
+    print(f"X2: {round(result.x[1], 2)} hours")
+else:
+    print("No solution")
